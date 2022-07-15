9b3a6b459daa4633874b65e35994fe7ddff83268
InTruder.Sec <75447837+InTruder-Sec@users.noreply.github.com>
2022-07-15 11:01:00 +0530

    Update find.sh

--- find.sh
+++ find.sh
@@ -1,2 +1,17 @@
-echo "Updating Database"
+#!/bin/bash
+
+echo "[+] Updating Database"
 curl "https://raw.githubusercontent.com/InTruder-Sec/Software-Papers/main/Software%20Papers.txt" -s -o Software_papers.txt
+echo "[+] Database Updated"
+echo "-----------------------------------------------------"
+echo "[+] Available Papers"
+echo "-----------------------------------------------------"
+
+while IFS= read -r line
+do
+  echo "$line"
+done < "Software_papers.txt"
+
+echo "Enter the Title and Paper no for more information: "
+read find
+echo "$find"
